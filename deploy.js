56c07e25d2cc2122e1dfc9a907852424bd7488d7
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-03-03 11:45:47 -0800

    boilerplate

---
+++ deploy.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+const seed = process.env.SEED;
+
+const provider = new HDWalletProvider(
+  seed,
+  "https://rinkeby.infura.io/v3/f34acffbd94f4f048f3afa44aa73551d"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
